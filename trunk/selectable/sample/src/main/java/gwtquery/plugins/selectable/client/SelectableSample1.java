f4022c713aaf757a01324cd6dc2d74a4dc7b0e19
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-07-31 18:02:07 +0000

    use GWT code style

--- trunk/selectable/sample/src/main/java/gwtquery/plugins/selectable/client/SelectableSample1.java
+++ trunk/selectable/sample/src/main/java/gwtquery/plugins/selectable/client/SelectableSample1.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.selectable.client;
 
 import static com.google.gwt.query.client.GQuery.$;
@@ -9,78 +24,79 @@ import com.google.gwt.query.client.Function;
 
 public class SelectableSample1 implements EntryPoint {
 
-    public void onModuleLoad() {
+  public void onModuleLoad() {
 
-	SelectableOptions options = initOptions();
-	    
-	$("#selectable").as(Selectable).selectable(options);
+    SelectableOptions options = initOptions();
 
+    $("#selectable").as(Selectable).selectable(options);
+
+  }
+
+  private SelectableOptions initOptions() {
+    SelectableOptions options = new SelectableOptions();
+    options.setOnSelected(new Function() {
+      @Override
+      public void f(Element e) {
+        fillSpan("selected", e.getInnerHTML());
+      }
+    });
+    options.setOnSelecting(new Function() {
+      @Override
+      public void f(Element e) {
+        fillSpan("selecting", e.getInnerHTML());
+      }
+    });
+    options.setOnUnselected(new Function() {
+      @Override
+      public void f(Element e) {
+        fillSpan("unselected", e.getInnerHTML());
+      }
+    });
+    options.setOnUnselecting(new Function() {
+      @Override
+      public void f(Element e) {
+        fillSpan("unselecting", e.getInnerHTML());
+      }
+    });
+    options.setOnStartSelection(new Function() {
+      @Override
+      public void f(Element e) {
+        $("#message").html("Selection started");
+        $("#selecting, #unselecting, #selected, #unselected").empty();
+      }
+    });
+    options.setOnStopSelection(new Function() {
+      @Override
+      public void f(Element e) {
+        final StringBuilder selectedItems = new StringBuilder();
+        $(".ui-selected", e).each(new Function() {
+          public void f(Element e) {
+            if (selectedItems.length() == 0) {
+              selectedItems.append(e.getInnerHTML());
+            } else {
+              selectedItems.append(", ").append(e.getInnerHTML());
+            }
+          };
+        });
+
+        $("#message").html("Selection ended.");
+        $("#totalSelected").html(selectedItems.toString());
+        $("#selecting, #unselecting").empty();
+      }
+    });
+
+    return options;
+  }
+
+  private void fillSpan(String id, String contentToAppend) {
+    String content = $("#" + id).html();
+    if (content == null || content.length() == 0) {
+      content = contentToAppend;
+    } else {
+      content += ", ";
+      content += contentToAppend;
     }
-
-    private SelectableOptions initOptions(){
-	SelectableOptions options = new SelectableOptions();
-	options.setOnSelected(new Function() {
-	    @Override
-	    public void f(Element e) {
-		fillSpan("selected", e.getInnerHTML());
-	    }
-	});
-	options.setOnSelecting(new Function() {
-	    @Override
-	    public void f(Element e) {
-		fillSpan("selecting", e.getInnerHTML());
-	    }
-	});
-	options.setOnUnselected(new Function() {
-	    @Override
-	    public void f(Element e) {
-		fillSpan("unselected", e.getInnerHTML());
-	    }
-	});
-	options.setOnUnselecting(new Function() {
-	    @Override
-	    public void f(Element e) {
-		fillSpan("unselecting", e.getInnerHTML());
-	    }
-	});
-	options.setOnStartSelection(new Function() {
-	    @Override
-	    public void f(Element e) {
-		$("#message").html("Selection started");
-		$("#selecting, #unselecting, #selected, #unselected").empty();
-	    }
-	});
-	options.setOnStopSelection(new Function() {
-	    @Override
-	    public void f(Element e) {
-		final StringBuilder selectedItems = new StringBuilder(); 
-		$(".ui-selected", e).each(new Function(){
-		    public void f(Element e) {
-			if (selectedItems.length() == 0){
-			    selectedItems.append(e.getInnerHTML());
-			}else{
-			    selectedItems.append(", ").append(e.getInnerHTML());
-			}
-		    };
-		});
-
-		$("#message").html("Selection ended.");
-		$("#totalSelected").html(selectedItems.toString());
-		$("#selecting, #unselecting").empty();
-	    }
-	});
-
-	return options;
-    }
-    private void fillSpan(String id, String contentToAppend) {
-	String content = $("#" + id).html();
-	if (content == null || content.length() == 0) {
-	    content = contentToAppend;
-	} else {
-	    content += ", ";
-	    content += contentToAppend;
-	}
-	$("#" + id).html(content);
-    }
+    $("#" + id).html(content);
+  }
 
 }
--- trunk/selectable/sample/src/main/java/gwtquery/plugins/selectable/client/SelectableSample2.java
+++ trunk/selectable/sample/src/main/java/gwtquery/plugins/selectable/client/SelectableSample2.java
@@ -1,8 +1,22 @@
+/*
+ * Copyright 2010 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.selectable.client;
 
 import static com.google.gwt.query.client.GQuery.$;
 import static gwtquery.plugins.selectable.client.Selectable.Selectable;
-import gwtquery.plugins.selectable.client.SelectableOptions.Tolerance;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
@@ -19,85 +33,86 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
+import gwtquery.plugins.selectable.client.SelectableOptions.Tolerance;
+
 public class SelectableSample2 implements EntryPoint {
 
-	public void onModuleLoad() {
-		
-		SelectableOptions o = new SelectableOptions();
-		o.setFilter("tr");
-		$("#selectable").as(Selectable).selectable(o);
-		
-		RootPanel.get("selectable-options").add(new SelectableOptionsPanel(o));
+  public void onModuleLoad() {
 
-	}
+    SelectableOptions o = new SelectableOptions();
+    o.setFilter("tr");
+    $("#selectable").as(Selectable).selectable(o);
 
+    RootPanel.get("selectable-options").add(new SelectableOptionsPanel(o));
 
-	public static class SelectableOptionsPanel extends Composite {
+  }
 
-		private static SelectableoptionsPanelUiBinder uiBinder = GWT
-				.create(SelectableoptionsPanelUiBinder.class);
-		
-		@UiTemplate(value = "SelectableOptionsPanel.ui.xml")
-		interface SelectableoptionsPanelUiBinder extends UiBinder<Widget, SelectableOptionsPanel> {
-		}
-		
-		private SelectableOptions options;
-		
-		@UiField
-		ListBox filterListBox;
-		@UiField
-		TextBox appendToBox;
-		@UiField
-		CheckBox disabledCheckBox;
-		@UiField
-		ListBox toleranceListBox;
+  public static class SelectableOptionsPanel extends Composite {
 
+    private static SelectableoptionsPanelUiBinder uiBinder = GWT
+        .create(SelectableoptionsPanelUiBinder.class);
 
-		public SelectableOptionsPanel(SelectableOptions o) {
-			options=o;
-			initWidget(uiBinder.createAndBindUi(this));
-			init();		
-			
-		}
+    @UiTemplate(value = "SelectableOptionsPanel.ui.xml")
+    interface SelectableoptionsPanelUiBinder extends
+        UiBinder<Widget, SelectableOptionsPanel> {
+    }
 
+    private SelectableOptions options;
 
-		private void init() {
-			
-			filterListBox.addItem("tr");
-			filterListBox.addItem("td");
-			filterListBox.setSelectedIndex(0);
-			
-			appendToBox.setValue(options.getAppendTo(), false);
-			
-			disabledCheckBox.setValue(options.isDisabled(), false);
-			
-			toleranceListBox.addItem(Tolerance.TOUCH.name());
-			toleranceListBox.addItem(Tolerance.FIT.name());
-			toleranceListBox.setSelectedIndex(0);
-			
-		}
-		
-		@UiHandler(value = "filterListBox")
-		public void onFilterChange(ChangeEvent e){
-			options.setFilter(filterListBox.getValue(filterListBox.getSelectedIndex()));
-		}
-		
-		@UiHandler(value = "appendToBox")
-		public void onAppendToChange(ValueChangeEvent<String> e){
-			options.setAppendTo(e.getValue());
-		}
-		
-		@UiHandler(value = "disabledCheckBox")
-		public void onDisabledChange(ValueChangeEvent<Boolean> e){
-			options.setDisabled(e.getValue());
-		}
-		
-		@UiHandler(value = "toleranceListBox")
-		public void onToleranceChange(ChangeEvent e){
-			String tolerance = toleranceListBox.getValue(toleranceListBox.getSelectedIndex());
-			options.setTolerance(Tolerance.valueOf(tolerance));
-		}
-		
-		
-	}
+    @UiField
+    ListBox filterListBox;
+    @UiField
+    TextBox appendToBox;
+    @UiField
+    CheckBox disabledCheckBox;
+    @UiField
+    ListBox toleranceListBox;
+
+    public SelectableOptionsPanel(SelectableOptions o) {
+      options = o;
+      initWidget(uiBinder.createAndBindUi(this));
+      init();
+
+    }
+
+    private void init() {
+
+      filterListBox.addItem("tr");
+      filterListBox.addItem("td");
+      filterListBox.setSelectedIndex(0);
+
+      appendToBox.setValue(options.getAppendTo(), false);
+
+      disabledCheckBox.setValue(options.isDisabled(), false);
+
+      toleranceListBox.addItem(Tolerance.TOUCH.name());
+      toleranceListBox.addItem(Tolerance.FIT.name());
+      toleranceListBox.setSelectedIndex(0);
+
+    }
+
+    @UiHandler(value = "filterListBox")
+    public void onFilterChange(ChangeEvent e) {
+      options.setFilter(filterListBox
+          .getValue(filterListBox.getSelectedIndex()));
+    }
+
+    @UiHandler(value = "appendToBox")
+    public void onAppendToChange(ValueChangeEvent<String> e) {
+      options.setAppendTo(e.getValue());
+    }
+
+    @UiHandler(value = "disabledCheckBox")
+    public void onDisabledChange(ValueChangeEvent<Boolean> e) {
+      options.setDisabled(e.getValue());
+    }
+
+    @UiHandler(value = "toleranceListBox")
+    public void onToleranceChange(ChangeEvent e) {
+      String tolerance = toleranceListBox.getValue(toleranceListBox
+          .getSelectedIndex());
+      options.setTolerance(Tolerance.valueOf(tolerance));
+    }
+
+  }
 }
