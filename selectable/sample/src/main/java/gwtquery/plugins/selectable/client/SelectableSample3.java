8454b6db93c92a88bfd64a4543dbdb29d5d5a20e
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-08-01 20:10:23 +0000

    finalize sample

--- selectable/sample/src/main/java/gwtquery/plugins/selectable/client/SelectableSample3.java
+++ selectable/sample/src/main/java/gwtquery/plugins/selectable/client/SelectableSample3.java
@@ -1,139 +1,160 @@
+/*
+ * Copyright 2010 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.selectable.client;
 
 import static com.google.gwt.query.client.GQuery.$;
 import static gwtquery.plugins.selectable.client.Selectable.Selectable;
-import gwtquery.plugins.selectable.client.SelectableOptions.Tolerance;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.uibinder.client.UiTemplate;
-import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.query.client.Function;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HTMLTable;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class SelectableSample3 implements EntryPoint {
 
-    	private static List<String> names;
-    	private static List<String> firstNames;
-    	
-    	static{
-    	    names = new ArrayList<String>();
-    	    names.add("Cobain");
-    	    names.add("Morisson");
-    	    names.add("Mercury");
-    	    names.add("Jackson");
-    	    names.add("Lennon");
-    	    names.add("Cash");
-            firstNames = new ArrayList<String>();
-            firstNames.add("Kurt");
-            firstNames.add("Jim");
-            firstNames.add("Freddy");
-            firstNames.add("Mickael");
-            firstNames.add("John");
-            firstNames.add("Johnny");
-    	}
-    	
-    	
-	public void onModuleLoad() {
-	    
-	    	FlexTable table = new FlexTable();
-	    	
-	    	initTable(table);
-	    	  
-		
-		SelectableOptions o = new SelectableOptions();
-		o.setFilter("tr");
-		$("#selectable").as(Selectable).selectable(o);
-		
-		RootPanel.get("selectable-options").add(new SelectableOptionsPanel(o));
+  private static final int ROW_NUMBER = 5;
+  private static final int COLUMN_NUMBER = 5;
 
-	}
+  private static class UnselectedWidget extends Composite {
 
+    String value;
+    int row;
+    int column;
 
-	private void initTable(FlexTable table) {
-	    // TODO Auto-generated method stub
-	    
-	}
+    public UnselectedWidget() {
+    }
 
+    public UnselectedWidget(String value, int row, int column) {
+      this.value = value;
+      this.row = row;
+      this.column = column;
+      init(null);
+    }
 
-	public static class SelectableOptionsPanel extends Composite {
+    public UnselectedWidget(SelectedWidget w) {
+      value = w.getValue();
+      row = w.getRow();
+      column = w.getColumn();
+      init(w);
+    }
 
-		private static SelectableoptionsPanelUiBinder uiBinder = GWT
-				.create(SelectableoptionsPanelUiBinder.class);
-		
-		@UiTemplate(value = "SelectableOptionsPanel.ui.xml")
-		interface SelectableoptionsPanelUiBinder extends UiBinder<Widget, SelectableOptionsPanel> {
-		}
-		
-		private SelectableOptions options;
-		
-		@UiField
-		ListBox filterListBox;
-		@UiField
-		TextBox appendToBox;
-		@UiField
-		CheckBox disabledCheckBox;
-		@UiField
-		ListBox toleranceListBox;
+    public int getColumn() {
+      return column;
+    }
 
+    public int getRow() {
+      return row;
+    }
 
-		public SelectableOptionsPanel(SelectableOptions o) {
-			options=o;
-			initWidget(uiBinder.createAndBindUi(this));
-			init();		
-			
-		}
+    public String getValue() {
+      return value;
+    }
 
+    protected String getInfo() {
+      return "I'm an instance of <b>*</b>Un<b>*</b>selected Widget class ";
+    }
 
-		private void init() {
-			
-			filterListBox.addItem("tr");
-			filterListBox.addItem("td");
-			filterListBox.setSelectedIndex(0);
-			
-			appendToBox.setValue(options.getAppendTo(), false);
-			
-			disabledCheckBox.setValue(options.isDisabled(), false);
-			
-			toleranceListBox.addItem(Tolerance.TOUCH.name());
-			toleranceListBox.addItem(Tolerance.FIT.name());
-			toleranceListBox.setSelectedIndex(0);
-			
-		}
-		
-		@UiHandler(value = "filterListBox")
-		public void onFilterChange(ChangeEvent e){
-			options.setFilter(filterListBox.getValue(filterListBox.getSelectedIndex()));
-		}
-		
-		@UiHandler(value = "appendToBox")
-		public void onAppendToChange(ValueChangeEvent<String> e){
-			options.setAppendTo(e.getValue());
-		}
-		
-		@UiHandler(value = "disabledCheckBox")
-		public void onDisabledChange(ValueChangeEvent<Boolean> e){
-			options.setDisabled(e.getValue());
-		}
-		
-		@UiHandler(value = "toleranceListBox")
-		public void onToleranceChange(ChangeEvent e){
-			String tolerance = toleranceListBox.getValue(toleranceListBox.getSelectedIndex());
-			options.setTolerance(Tolerance.valueOf(tolerance));
-		}
-		
-		
-	}
+    protected void init(Widget w) {
+      VerticalPanel panel = new VerticalPanel();
+      panel.add(new HTML(value));
+      panel.add(new HTML(getInfo()));
+      panel.getElement().setId(row + "_" + column);
+      initWidget(panel);
+
+      if (w != null) {
+        addStyleName(w.getStyleName());
+      } else {
+        addStyleName("can-be-selected");
+      }
+
+    }
+
+  }
+
+  private static class SelectedWidget extends UnselectedWidget {
+
+    public SelectedWidget(UnselectedWidget w) {
+      value = w.getValue();
+      row = w.getRow();
+      column = w.getColumn();
+      init(w);
+    }
+
+    protected String getInfo() {
+      return "I'm an instance of Selected Widget class ";
+    }
+
+  }
+
+  public void onModuleLoad() {
+
+    final Grid table = new Grid(ROW_NUMBER, COLUMN_NUMBER);
+    initTable(table);
+    RootPanel.get("table").add(table);
+
+    // the table is now attached to the DOM, we can use Gquery
+    // init options
+    SelectableOptions o = new SelectableOptions();
+    o.setFilter(".can-be-selected");
+    o.setOnSelected(new Function() {
+      @Override
+      public void f(Element e) {
+        UnselectedWidget w = (UnselectedWidget) getRelatedWidget(e, table);
+        SelectedWidget selected = new SelectedWidget(w);
+        table.setWidget(w.getRow(), w.getColumn(), selected);
+      }
+    });
+    o.setOnUnselected(new Function() {
+      @Override
+      public void f(Element e) {
+        SelectedWidget w = (SelectedWidget) getRelatedWidget(e, table);
+        UnselectedWidget unselected = new UnselectedWidget(w);
+        table.setWidget(w.getRow(), w.getColumn(), unselected);
+      }
+    });
+    // that all folks !
+    $(table.getElement()).as(Selectable).selectable(o);
+
+  }
+
+  private Widget getRelatedWidget(Element tableElement, final HTMLTable table) {
+    String id = tableElement.getId();
+    int row = new Integer(id.substring(0, 1));
+    int column = new Integer(id.substring(2, 3));
+    return table.getWidget(row, column);
+  }
+
+  private void initTable(Grid table) {
+
+    for (int i = 0; i < ROW_NUMBER; i++) {
+      for (int j = 0; j < COLUMN_NUMBER; j++) {
+        UnselectedWidget w = new UnselectedWidget("" + i + j, i, j);
+        table.setWidget(i, j, w);
+      }
+    }
+    
+    table.setCellSpacing(5);
+    table.setBorderWidth(1);
+
+  }
+
 }
