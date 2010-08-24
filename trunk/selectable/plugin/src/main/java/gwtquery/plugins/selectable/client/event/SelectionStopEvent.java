5d5460803b879bc71d53b2eb8344fccb70f45dc3
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-08-24 19:38:02 +0000

    add license term and javadoc

--- trunk/selectable/plugin/src/main/java/gwtquery/plugins/selectable/client/event/SelectionStopEvent.java
+++ trunk/selectable/plugin/src/main/java/gwtquery/plugins/selectable/client/event/SelectionStopEvent.java
@@ -1,35 +1,57 @@
+/*
+ * Copyright 2010 The gwtquery plugins team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.selectable.client.event;
 
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.shared.EventHandler;
 import com.google.gwt.event.shared.GwtEvent;
 
-public class SelectionStopEvent extends GwtEvent<SelectionStopEvent.SelectionStopEventHandler> {
+/**
+ * Event fired when the selection stops.
+ * 
+ * @author Julien Dramaix (julien.dramaix@gmail.com)
+ * 
+ */
+public class SelectionStopEvent extends
+    GwtEvent<SelectionStopEvent.SelectionStopEventHandler> {
 
-  public interface SelectionStopEventHandler extends EventHandler{
+  public interface SelectionStopEventHandler extends EventHandler {
     public void onSelectionStop(SelectionStopEvent event);
   }
-  
+
   public static Type<SelectionStopEventHandler> TYPE = new Type<SelectionStopEventHandler>();
-  
+
   private Element selectable;
-  
+
   public SelectionStopEvent(Element e) {
     selectable = e;
-  }
-  
-  @Override
-  protected void dispatch(SelectionStopEventHandler handler) {
-    handler.onSelectionStop(this);
   }
 
   @Override
   public Type<SelectionStopEventHandler> getAssociatedType() {
     return TYPE;
   }
-  
+
   public Element getSelectable() {
     return selectable;
+  }
+
+  @Override
+  protected void dispatch(SelectionStopEventHandler handler) {
+    handler.onSelectionStop(this);
   }
 
 }
--- trunk/selectable/plugin/src/main/java/gwtquery/plugins/selectable/client/event/UnselectingEvent.java
+++ trunk/selectable/plugin/src/main/java/gwtquery/plugins/selectable/client/event/UnselectingEvent.java
@@ -1,35 +1,56 @@
+/*
+ * Copyright 2010 The gwtquery plugins team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.selectable.client.event;
 
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.shared.EventHandler;
 import com.google.gwt.event.shared.GwtEvent;
 
-public class UnselectingEvent extends GwtEvent<UnselectingEvent.UnselectingEventHandler> {
+/**
+ * Event fired when an element is unselecting.
+ * 
+ * @author Julien Dramaix (julien.dramaix@gmail.com)
+ * 
+ */
+public class UnselectingEvent extends
+    GwtEvent<UnselectingEvent.UnselectingEventHandler> {
 
-  public interface UnselectingEventHandler extends EventHandler{
+  public interface UnselectingEventHandler extends EventHandler {
     public void onUnselecting(UnselectingEvent event);
   }
-  
+
   public static Type<UnselectingEventHandler> TYPE = new Type<UnselectingEventHandler>();
-  
+
   private Element unselectingElement;
-  
+
   public UnselectingEvent(Element e) {
     unselectingElement = e;
-  }
-  
-  @Override
-  protected void dispatch(UnselectingEventHandler handler) {
-    handler.onUnselecting(this);
   }
 
   @Override
   public Type<UnselectingEventHandler> getAssociatedType() {
     return TYPE;
   }
-  
+
   public Element getUnselectingElement() {
     return unselectingElement;
   }
 
+  @Override
+  protected void dispatch(UnselectingEventHandler handler) {
+    handler.onUnselecting(this);
+  }
 }
