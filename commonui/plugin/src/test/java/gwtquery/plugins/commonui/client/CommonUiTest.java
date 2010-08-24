149ecf636423303fffffd80122f2daf389b852c0
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-08-24 20:01:05 +0000

    Initial import.

---
+++ commonui/plugin/src/test/java/gwtquery/plugins/commonui/client/CommonUiTest.java
@@ -0,0 +1,16 @@
+package gwtquery.plugins.commonui.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * Test class for CommonUi plugin
+ */
+public class CommonUiTest extends GWTTestCase {
+
+  public String getModuleName() {
+    return "gwtquery.plugins.commonui.CommonUi";
+  }
+
+  public void testCommonUiApply() {
+  }
+}
