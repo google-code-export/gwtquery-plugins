7f00292865984ea9307bfe0d705138486c5975f7
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-08-30 22:05:27 +0000

    initial revision

---
+++ trunk/commonui/plugin/src/main/java/gwtquery/plugins/commonui/client/GQueryUi.java
@@ -0,0 +1,73 @@
+package gwtquery.plugins.commonui.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NodeList;
+import com.google.gwt.query.client.GQuery;
+import com.google.gwt.query.client.JSArray;
+import com.google.gwt.user.client.Event;
+
+/**
+ * GWT clone of jQueryUi-core
+ * 
+ * @author Julien Dramaix (julien.dramaix@gmail.com)
+ *
+ */
+public class GQueryUi extends GQuery {
+
+  public static Class<GQueryUi> GQueryUi = GQueryUi.class;
+  
+  private GQueryUiImpl impl = GWT.create(GQueryUiImpl.class);
+  
+  public GQueryUi() {
+    super();
+  }
+
+  public GQueryUi(Element element) {
+    super(element);
+  }
+
+  public GQueryUi(GQuery gq) {
+    super(gq);
+  }
+
+  public GQueryUi(JSArray elements) {
+    super(elements);
+  }
+
+  public GQueryUi(NodeList<Element> list) {
+    super(list);
+  }
+  
+  /**
+   * Simulate event.pageX from jQuery
+   * 
+   * @param mouseEvent
+   * @return the mouse x-position in the page
+   */
+  public int pageX(Event mouseEvent) {
+    int pageX = mouseEvent.getClientX() + document.getScrollLeft();
+    return pageX;
+  }
+
+  /**
+   * Simulate event.pageY from jQuery
+   * 
+   * @param mouseEvent
+   * @return the mouse y-position in the page
+   */
+  public int pageY(Event mouseEvent) {
+    int pageY = mouseEvent.getClientY() + document.getScrollTop();
+    return pageY;
+  }
+  
+  /**
+   * this function returns the immediate scrolling parent.
+   *  
+   * @return the immediate scrolling parent
+   */
+  public GQuery scrollParent(){
+   return impl.scrollParent(this);
+  }
+
+}
---
+++ trunk/commonui/plugin/src/main/java/gwtquery/plugins/commonui/client/GQueryUiImpl.java
@@ -0,0 +1,47 @@
+package gwtquery.plugins.commonui.client;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.query.client.GQuery;
+import com.google.gwt.query.client.Predicate;
+
+public class GQueryUiImpl {
+
+  public GQuery scrollParent(final GQueryUi gQueryUi) {
+    GQuery scrollParent;
+    
+    if (gQueryUi.css("position").matches("(fixed)")){
+      return GQuery.$(GQuery.document);
+    }
+    
+    if (scrollParentPositionTest(gQueryUi)){
+      scrollParent = gQueryUi.parents().filter(new Predicate() {
+        
+        public boolean f(Element e, int index) {
+          GQuery $e = GQuery.$(e);
+          String position = $e.css("position", true);
+          //concatenate all overflow css rules
+          String overflow = $e.css("overflow", true)+$e.css("overflow-x", true)+$e.css("overflow-y", true);
+          return position.matches("(relative|absolute|fixed)") && overflow.matches("(auto|scroll)");
+        }
+      });
+    
+    }else{
+      scrollParent = gQueryUi.parents().filter(new Predicate() {
+        
+        public boolean f(Element e, int index) {
+          GQuery $e = GQuery.$(e);
+          //concatenate all overflow css rules
+          String overflow = $e.css("overflow", true)+$e.css("overflow-x", true)+$e.css("overflow-y", true);
+          return overflow.matches("(auto|scroll)");
+        }
+      });
+    }
+    return scrollParent.length() > 0 ? new GQuery(scrollParent.get(0)) : GQuery.$(GQuery.document);
+
+  }
+  
+  protected boolean scrollParentPositionTest(GQueryUi gQueryUi){
+    return gQueryUi.css("position").matches("(absolute)");
+  }
+
+}
