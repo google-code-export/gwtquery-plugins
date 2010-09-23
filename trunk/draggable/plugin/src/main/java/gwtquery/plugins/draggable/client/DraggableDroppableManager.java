bff7527eae6258aa1ee72ec65d28c8f02264e9d1
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-09-23 08:58:10 +0000

    Use DraggableHandler instead of DraggableInfo

--- trunk/draggable/plugin/src/main/java/gwtquery/plugins/draggable/client/DraggableDroppableManager.java
+++ trunk/draggable/plugin/src/main/java/gwtquery/plugins/draggable/client/DraggableDroppableManager.java
@@ -4,29 +4,43 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.Event;
 
+import java.util.Collection;
+
 public class DraggableDroppableManager {
 
-  private static DraggableDroppableManager INSTANCE = GWT.create(DraggableDroppableManager.class);
-  
-  public static DraggableDroppableManager getInstance(){
+  private static DraggableDroppableManager INSTANCE = GWT
+      .create(DraggableDroppableManager.class);
+
+  public static DraggableDroppableManager getInstance() {
     return INSTANCE;
   }
-  
-  public void drag(Element draggable, Event e){};
-  public boolean drop(Element draggable, Event e){
+
+  public void drag(Element draggable, DraggableOptions options, Event e) {
+  };
+
+  public boolean drop(Element draggable, DraggableOptions options, Event e) {
     return false;
   };
-  public void prepareOffset(Element draggable, Event e){};
-  
-  
-  public boolean isHandleDroppable(){
+
+  public void prepareOffset(Element draggable, DraggableOptions options, Event e) {
+  };
+
+  public boolean isHandleDroppable() {
     return false;
   }
 
-  public void setCurrentDraggable(Element draggable) {}
+  public void setCurrentDraggable(Element draggable) {
+  }
+
   public Element getCurrentDraggable() {
     return null;
   }
   
-  
+  public Collection<Element> getDroppablesByScope(String scope){
+    return null;
+  }
+
+  public void setDroppable(Element droppable, String scope) {
+  }
+
 }
