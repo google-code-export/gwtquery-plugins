d81b2594ec3cbfd5387052f7e99081d6ad139b60
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-10-25 06:24:39 +0000

    remove unused import

--- trunk/draggable/plugin/src/main/java/gwtquery/plugins/draggable/client/plugins/ZIndexPlugin.java
+++ trunk/draggable/plugin/src/main/java/gwtquery/plugins/draggable/client/plugins/ZIndexPlugin.java
@@ -17,7 +17,6 @@ package gwtquery.plugins.draggable.client.plugins;
 
 import static com.google.gwt.query.client.GQuery.$;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.query.client.GQuery;
 
