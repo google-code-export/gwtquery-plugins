cb7fec5d60ab1ca8cf9631a84c4d1bfcf0e5c134
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-11-06 08:23:16 +0000

    Add copyright and start javadoc

--- droppable/plugin/src/main/java/gwtquery/plugins/droppable/client/DraggableDroppableManagerImpl.java
+++ droppable/plugin/src/main/java/gwtquery/plugins/droppable/client/DraggableDroppableManagerImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 The gwtquery plugins team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.droppable.client;
 
 import static com.google.gwt.query.client.GQuery.$;
@@ -18,6 +33,12 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Implementation of the {@link DraggableDroppableManager} for drop operations
+ * 
+ * @author Julien Dramaix (julien.dramaix@gmail.com)
+ *
+ */
 public class DraggableDroppableManagerImpl extends DraggableDroppableManager {
 
   private Element currentDraggable;
--- droppable/plugin/src/main/java/gwtquery/plugins/droppable/client/events/AbstractDroppableEvent.java
+++ droppable/plugin/src/main/java/gwtquery/plugins/droppable/client/events/AbstractDroppableEvent.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 The gwtquery plugins team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.droppable.client.events;
 
 
@@ -8,6 +23,13 @@ import com.google.gwt.event.shared.GwtEvent;
 import gwtquery.plugins.draggable.client.gwt.DraggableWidget;
 import gwtquery.plugins.droppable.client.gwt.DroppableWidget;
 
+/**
+ * Abstract class for all droppable event
+ * 
+ * @author Julien Dramaix (julien.dramaix@gmail.com)
+ *
+ * @param <H>
+ */
 public abstract class AbstractDroppableEvent<H extends EventHandler> extends
 		GwtEvent<H> {
 
