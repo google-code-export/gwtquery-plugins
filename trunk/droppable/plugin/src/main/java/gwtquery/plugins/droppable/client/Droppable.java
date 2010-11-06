c2db038b26c48e976786a741fde4efb249b4277a
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-11-06 08:23:16 +0000

    Add copyright and start javadoc

--- trunk/droppable/plugin/src/main/java/gwtquery/plugins/droppable/client/Droppable.java
+++ trunk/droppable/plugin/src/main/java/gwtquery/plugins/droppable/client/Droppable.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 The gwtquery plugins team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.droppable.client;
 
 import com.google.gwt.dom.client.Element;
@@ -11,7 +26,7 @@ import gwtquery.plugins.commonui.client.GQueryUi;
 import gwtquery.plugins.draggable.client.DraggableDroppableManager;
 
 /**
- * Droppable for GwtQuery
+ * Droppable plugin for GwtQuery
  */
 public class Droppable extends GQueryUi {
 
--- trunk/droppable/plugin/src/main/java/gwtquery/plugins/droppable/client/events/DragAndDropContext.java
+++ trunk/droppable/plugin/src/main/java/gwtquery/plugins/droppable/client/events/DragAndDropContext.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 The gwtquery plugins team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.droppable.client.events;
 
 import com.google.gwt.dom.client.Element;
@@ -6,7 +21,7 @@ import com.google.gwt.query.client.GQuery.Offset;
 import gwtquery.plugins.draggable.client.DraggableHandler;
 
 /**
- * 
+ * Object containing usefull information on the drag and drop operations.
  * @author Julien Dramaix (julien.dramaix@gmail.com)
  * 
  */
