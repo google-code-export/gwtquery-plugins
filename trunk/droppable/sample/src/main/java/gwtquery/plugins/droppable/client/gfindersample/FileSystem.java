464f7534ae891ef0e9349429e2394767683cff18
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-11-22 15:23:07 +0000

    finalize GFinder sample

--- trunk/droppable/sample/src/main/java/gwtquery/plugins/droppable/client/gfindersample/FileSystem.java
+++ trunk/droppable/sample/src/main/java/gwtquery/plugins/droppable/client/gfindersample/FileSystem.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2010 Google Inc.
- *
+ * Copyright 2010 The gwtquery plugins team.
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -19,25 +19,17 @@ import com.google.gwt.user.client.Random;
 import com.google.gwt.view.client.ListDataProvider;
 import com.google.gwt.view.client.ProvidesKey;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
- * The data source for contact information used in the sample. This majority of
- * the code below comes from the GWT showcase :
- * http://gwt.google.com/samples/Showcase/Showcase.html#!CwCellList
+ * Class simulating the file system.
+ * 
+ * @author Julien Dramaix (julien.dramaix@gmail.com)
  * 
  */
 public class FileSystem {
-
-  /**
-   * File type
-   */
-  public static enum FileType {
-    DIRECTORY, FILE;
-  }
 
   /**
    * File object
@@ -45,7 +37,7 @@ public class FileSystem {
   public static class File implements Comparable<File> {
 
     /**
-     * The key provider that provides the unique ID of a contact.
+     * The key provider that provides the unique ID of a file.
      */
     public static final ProvidesKey<File> KEY_PROVIDER = new ProvidesKey<File>() {
       public Object getKey(File item) {
@@ -55,10 +47,10 @@ public class FileSystem {
 
     private static int nextId = 0;
 
-    private String name;
-    private FileType type;
     private final int id;
+    private String name;
     private File parent;
+    private FileType type;
 
     public File(FileType type) {
       this.type = type;
@@ -84,20 +76,25 @@ public class FileSystem {
       return false;
     }
 
-    @Override
-    public int hashCode() {
-      return id;
+    /**
+     * @return the unique ID of the file
+     */
+    public int getId() {
+      return this.id;
     }
 
     /**
-     * @return the contact's address
+     * @return the file's address
      */
     public String getName() {
       return name;
     }
 
-    public void setName(String name) {
-      this.name = name;
+    /**
+     * @return the parent directory or null if it is root
+     */
+    public File getParent() {
+      return parent;
     }
 
     /**
@@ -107,15 +104,13 @@ public class FileSystem {
       return type;
     }
 
-    /**
-     * @return the unique ID of the contact
-     */
-    public int getId() {
-      return this.id;
+    @Override
+    public int hashCode() {
+      return id;
     }
 
-    public File getParent() {
-      return parent;
+    public void setName(String name) {
+      this.name = name;
     }
 
     public void setParent(File parent) {
@@ -123,77 +118,84 @@ public class FileSystem {
     }
 
   }
-  
-  private static final String ROOT_DIRECTORY = "Pictures";
-
- 
-  private static final String[] USER_DIRECTORY_NAME = {  "Mary", "Patricia", "Linda", "Barbara", "Elizabeth", "Jennifer", "Maria",
-    "Susan", "Margaret", "Dorothy", "Lisa", "Nancy", "Karen", "Betty",
-    "Helen", "Sandra", "Donna", "Carol", "Ruth", "Sharon", "Michelle",
-    "Laura", "Sarah", "Kimberly", "Deborah", "Jessica", "Shirley", "Cynthia",
-    "Angela", "Melissa", "Brenda", "Amy", "Anna", "Rebecca", "Virginia",
-    "Kathleen", "Pamela", "Martha", "Debra", "Amanda", "Stephanie", "Carolyn",
-    "Christine", "Marie", "Janet", "Catherine", "Frances", "Ann", "Joyce",
-    "Diane", "Alice", "Julie", "Heather", "Teresa", "Doris", "Gloria",
-    "Evelyn", "Jean", "Cheryl", "Mildred", "Katherine", "Joan", "Ashley",
-    "Judith", "Rose", "Janice", "Kelly", "Nicole", "Judy", "Christina",
-    "Kathy", "Theresa", "Beverly", "Denise", "Tammy", "Irene", "Jane", "Lori",
-    "Rachel", "Marilyn", "Andrea", "Kathryn", "Louise", "Sara", "Anne",
-    "Jacqueline", "Wanda", "Bonnie", "Julia", "Ruby", "Lois", "Tina",
-    "Phyllis", "Norma", "Paula", "Diana", "Annie", "Lillian", "Emily",
-    "Robin", "Peggy", "Crystal", "Gladys", "Rita", "Dawn", "Connie",
-    "Florence", "Tracy", "Edna", "Tiffany", "Carmen", "Rosa", "Cindy",
-    "Grace", "Wendy", "Victoria", "Edith", "Kim", "Sherry", "Sylvia",
-    "Josephine", "Thelma", "Shannon", "Sheila", "Ethel", "Ellen", "Elaine",
-    "Marjorie", "Carrie", "Charlotte", "Monica", "Esther", "Pauline", "Emma",
-    "Juanita", "Anita", "Rhonda", "Hazel", "Amber", "Eva", "Debbie", "April",
-    "Leslie", "Clara", "Lucille", "Jamie", "Joanne", "Eleanor", "Valerie",
-    "Danielle", "Megan", "Alicia", "Suzanne", "Michele", "Gail", "Bertha",
-    "Darlene", "Veronica", "Jill", "Erin", "Geraldine", "Lauren", "Cathy",
-    "Joann", "Lorraine", "Lynn", "Sally", "Regina", "Erica", "Beatrice",
-    "Dolores", "Bernice", "Audrey", "Yvonne", "Annette", "June", "Samantha",
-    "Marion", "Dana", "Stacy", "Ana", "Renee", "Ida", "Vivian", "Roberta",
-    "Holly", "Brittany", "Melanie", "Loretta", "Yolanda", "Jeanette",
-    "Laurie", "Katie", "Kristen", "Vanessa", "Alma", "Sue", "Elsie", "Beth",
-    "Jeanne", "James", "John", "Robert", "Michael", "William", "David", "Richard",
-    "Charles", "Joseph", "Thomas", "Christopher", "Daniel", "Paul", "Mark",
-    "Donald", "George", "Kenneth", "Steven", "Edward", "Brian", "Ronald",
-    "Anthony", "Kevin", "Jason", "Matthew", "Gary", "Timothy", "Jose",
-    "Larry", "Jeffrey", "Frank", "Scott", "Eric", "Stephen", "Andrew",
-    "Raymond", "Gregory", "Joshua", "Jerry", "Dennis", "Walter", "Patrick",
-    "Peter", "Harold", "Douglas", "Henry", "Carl", "Arthur", "Ryan", "Roger",
-    "Joe", "Juan", "Jack", "Albert", "Jonathan", "Justin", "Terry", "Gerald",
-    "Keith", "Samuel", "Willie", "Ralph", "Lawrence", "Nicholas", "Roy",
-    "Benjamin", "Bruce", "Brandon", "Adam", "Harry", "Fred", "Wayne", "Billy",
-    "Steve", "Louis", "Jeremy", "Aaron", "Randy", "Howard", "Eugene",
-    "Carlos", "Russell", "Bobby", "Victor", "Martin", "Ernest", "Phillip",
-    "Todd", "Jesse", "Craig", "Alan", "Shawn", "Clarence", "Sean", "Philip",
-    "Chris", "Johnny", "Earl", "Jimmy", "Antonio", "Danny", "Bryan", "Tony",
-    "Luis", "Mike", "Stanley", "Leonard", "Nathan", "Dale", "Manuel",
-    "Rodney", "Curtis", "Norman", "Allen", "Marvin", "Vincent", "Glenn",
-    "Jeffery", "Travis", "Jeff", "Chad", "Jacob", "Lee", "Melvin", "Alfred",
-    "Kyle", "Francis", "Bradley", "Jesus", "Herbert", "Frederick", "Ray",
-    "Joel", "Edwin", "Don", "Eddie", "Ricky", "Troy", "Randall", "Barry",
-    "Alexander", "Bernard", "Mario", "Leroy", "Francisco", "Marcus",
-    "Micheal", "Theodore", "Clifford", "Miguel", "Oscar", "Jay", "Jim", "Tom",
-    "Calvin", "Alex", "Jon", "Ronnie", "Bill", "Lloyd", "Tommy", "Leon",
-    "Derek", "Warren", "Darrell", "Jerome", "Floyd", "Leo", "Alvin", "Tim",
-    "Wesley", "Gordon", "Dean", "Greg", "Jorge", "Dustin", "Pedro", "Derrick",
-    "Dan", "Lewis", "Zachary", "Corey", "Herman", "Maurice", "Vernon",
-    "Roberto", "Clyde", "Glen", "Hector", "Shane", "Ricardo", "Sam", "Rick",
-    "Lester", "Brent", "Ramon", "Charlie", "Tyler", "Gilbert", "Gene" };
-
- 
-  private static int MAX_FILE_NBR = 50;
-  private static int MAX_DIR_NBR = 40;
 
   /**
-   * The singleton instance of the database.
+   * File type
+   */
+  public static enum FileType {
+    DIRECTORY, FILE;
+  }
+
+  /**
+   * The singleton instance.
    */
   private static FileSystem instance;
 
+  private static int MAX_DIR_NBR = 40;
+
+  private static int MAX_FILE_NBR = 50;
+  private static final String ROOT_DIRECTORY = "Pictures";
+
+  private static final String[] USER_DIRECTORY_NAME = { "Mary", "Patricia",
+      "Linda", "Barbara", "Elizabeth", "Jennifer", "Maria", "Susan",
+      "Margaret", "Dorothy", "Lisa", "Nancy", "Karen", "Betty", "Helen",
+      "Sandra", "Donna", "Carol", "Ruth", "Sharon", "Michelle", "Laura",
+      "Sarah", "Kimberly", "Deborah", "Jessica", "Shirley", "Cynthia",
+      "Angela", "Melissa", "Brenda", "Amy", "Anna", "Rebecca", "Virginia",
+      "Kathleen", "Pamela", "Martha", "Debra", "Amanda", "Stephanie",
+      "Carolyn", "Christine", "Marie", "Janet", "Catherine", "Frances", "Ann",
+      "Joyce", "Diane", "Alice", "Julie", "Heather", "Teresa", "Doris",
+      "Gloria", "Evelyn", "Jean", "Cheryl", "Mildred", "Katherine", "Joan",
+      "Ashley", "Judith", "Rose", "Janice", "Kelly", "Nicole", "Judy",
+      "Christina", "Kathy", "Theresa", "Beverly", "Denise", "Tammy", "Irene",
+      "Jane", "Lori", "Rachel", "Marilyn", "Andrea", "Kathryn", "Louise",
+      "Sara", "Anne", "Jacqueline", "Wanda", "Bonnie", "Julia", "Ruby", "Lois",
+      "Tina", "Phyllis", "Norma", "Paula", "Diana", "Annie", "Lillian",
+      "Emily", "Robin", "Peggy", "Crystal", "Gladys", "Rita", "Dawn", "Connie",
+      "Florence", "Tracy", "Edna", "Tiffany", "Carmen", "Rosa", "Cindy",
+      "Grace", "Wendy", "Victoria", "Edith", "Kim", "Sherry", "Sylvia",
+      "Josephine", "Thelma", "Shannon", "Sheila", "Ethel", "Ellen", "Elaine",
+      "Marjorie", "Carrie", "Charlotte", "Monica", "Esther", "Pauline", "Emma",
+      "Juanita", "Anita", "Rhonda", "Hazel", "Amber", "Eva", "Debbie", "April",
+      "Leslie", "Clara", "Lucille", "Jamie", "Joanne", "Eleanor", "Valerie",
+      "Danielle", "Megan", "Alicia", "Suzanne", "Michele", "Gail", "Bertha",
+      "Darlene", "Veronica", "Jill", "Erin", "Geraldine", "Lauren", "Cathy",
+      "Joann", "Lorraine", "Lynn", "Sally", "Regina", "Erica", "Beatrice",
+      "Dolores", "Bernice", "Audrey", "Yvonne", "Annette", "June", "Samantha",
+      "Marion", "Dana", "Stacy", "Ana", "Renee", "Ida", "Vivian", "Roberta",
+      "Holly", "Brittany", "Melanie", "Loretta", "Yolanda", "Jeanette",
+      "Laurie", "Katie", "Kristen", "Vanessa", "Alma", "Sue", "Elsie", "Beth",
+      "Jeanne", "James", "John", "Robert", "Michael", "William", "David",
+      "Richard", "Charles", "Joseph", "Thomas", "Christopher", "Daniel",
+      "Paul", "Mark", "Donald", "George", "Kenneth", "Steven", "Edward",
+      "Brian", "Ronald", "Anthony", "Kevin", "Jason", "Matthew", "Gary",
+      "Timothy", "Jose", "Larry", "Jeffrey", "Frank", "Scott", "Eric",
+      "Stephen", "Andrew", "Raymond", "Gregory", "Joshua", "Jerry", "Dennis",
+      "Walter", "Patrick", "Peter", "Harold", "Douglas", "Henry", "Carl",
+      "Arthur", "Ryan", "Roger", "Joe", "Juan", "Jack", "Albert", "Jonathan",
+      "Justin", "Terry", "Gerald", "Keith", "Samuel", "Willie", "Ralph",
+      "Lawrence", "Nicholas", "Roy", "Benjamin", "Bruce", "Brandon", "Adam",
+      "Harry", "Fred", "Wayne", "Billy", "Steve", "Louis", "Jeremy", "Aaron",
+      "Randy", "Howard", "Eugene", "Carlos", "Russell", "Bobby", "Victor",
+      "Martin", "Ernest", "Phillip", "Todd", "Jesse", "Craig", "Alan", "Shawn",
+      "Clarence", "Sean", "Philip", "Chris", "Johnny", "Earl", "Jimmy",
+      "Antonio", "Danny", "Bryan", "Tony", "Luis", "Mike", "Stanley",
+      "Leonard", "Nathan", "Dale", "Manuel", "Rodney", "Curtis", "Norman",
+      "Allen", "Marvin", "Vincent", "Glenn", "Jeffery", "Travis", "Jeff",
+      "Chad", "Jacob", "Lee", "Melvin", "Alfred", "Kyle", "Francis", "Bradley",
+      "Jesus", "Herbert", "Frederick", "Ray", "Joel", "Edwin", "Don", "Eddie",
+      "Ricky", "Troy", "Randall", "Barry", "Alexander", "Bernard", "Mario",
+      "Leroy", "Francisco", "Marcus", "Micheal", "Theodore", "Clifford",
+      "Miguel", "Oscar", "Jay", "Jim", "Tom", "Calvin", "Alex", "Jon",
+      "Ronnie", "Bill", "Lloyd", "Tommy", "Leon", "Derek", "Warren", "Darrell",
+      "Jerome", "Floyd", "Leo", "Alvin", "Tim", "Wesley", "Gordon", "Dean",
+      "Greg", "Jorge", "Dustin", "Pedro", "Derrick", "Dan", "Lewis", "Zachary",
+      "Corey", "Herman", "Maurice", "Vernon", "Roberto", "Clyde", "Glen",
+      "Hector", "Shane", "Ricardo", "Sam", "Rick", "Lester", "Brent", "Ramon",
+      "Charlie", "Tyler", "Gilbert", "Gene" };
+
   /**
-   * Get the singleton instance of the contact database.
+   * Get the singleton instance.
    * 
    * @return the singleton instance
    */
@@ -205,7 +207,7 @@ public class FileSystem {
   }
 
   /**
-   * The provider that holds the list of file in a directory.
+   * The provider that holds the list of files in a directory.
    */
   private Map<File, ListDataProvider<File>> filesByDirectory = new HashMap<File, ListDataProvider<File>>();
   private ListDataProvider<File> rootDirectories = new ListDataProvider<File>();
@@ -214,114 +216,105 @@ public class FileSystem {
    * Construct the file system.
    */
   private FileSystem() {
-   
-      File rootDirectory = new File(FileType.DIRECTORY);
-      rootDirectory.setName(ROOT_DIRECTORY);
-      
-      generateDirTree(rootDirectory);
-      
-      rootDirectories.getList().add(rootDirectory);
-      
 
-  }
-  
-  private void generateDirTree(File rootDirectory){
-    //generate user directories
-    ListDataProvider<File> files = new ListDataProvider<File>();
-    
-    for (int i = 0; i < Math.max(10, Random.nextInt(20)) ; i++){
-      File f = new File(FileType.DIRECTORY);
-      f.setName(nextValue(USER_DIRECTORY_NAME));
-      f.setParent(rootDirectory);
-      files.getList().add(f);
-      //generate Event sub directories
-      generateDirContent(f, 0, Math.max(10,Random.nextInt(MAX_DIR_NBR)));
-      
-    }
-    filesByDirectory.put(rootDirectory, files);
-    
-    
-  }
+    File rootDirectory = new File(FileType.DIRECTORY);
+    rootDirectory.setName(ROOT_DIRECTORY);
 
-  private void generateDirContent(File root, int fileNbr, int subDirNbr) {
-    
-    assert root.getType() == FileType.DIRECTORY;
-    
-    ListDataProvider<File> files = new ListDataProvider<File>();
-    
-    for (int i = 0; i < subDirNbr; i++){
-      File f = new File(FileType.DIRECTORY);
-      f.setName(root.getName()+"Event"+i);
-      f.setParent(root);
-      files.getList().add(f);
-      
-      generateDirContent(f, Math.max(10, Random.nextInt(MAX_FILE_NBR)), 0);
-    }
+    generateDirTree(rootDirectory);
 
-    for (int i = 0; i < fileNbr; i++){
-      File f = new File(FileType.FILE);
-      f.setName(root.getName()+"PICT"+i+".jpg");
-      f.setParent(root);
-      files.getList().add(f);
-    }
-    
-    filesByDirectory.put(root, files);
+    rootDirectories.getList().add(rootDirectory);
 
   }
 
-  
   /**
-   * move file
+   * Return the RootDirectories
+   * @return
+   */
+  public ListDataProvider<File> getRootDirectories() {
+    return rootDirectories;
+
+  }
+
+  /**
+   * List the files in the directory <code>dir</code>
    * 
-   * @param contact
-   *          the contact to add.
+   * @param dir
+   * 
+   */
+  public ListDataProvider<File> ls(File dir) {
+    return filesByDirectory.get(dir);
+  }
+
+  /**
+   * Move the file <code>file</code> to the directory <code>destination</code>
+   * 
    */
   public void moveFile(File file, File destination) {
     assert destination.getType() == FileType.DIRECTORY;
-    
+
     File oldDestination = file.getParent();
-    List<File> previousList = getFilesDataProvider(oldDestination).getList();
-    List<File> newList = getFilesDataProvider(destination).getList();
-    
+    List<File> previousList = ls(oldDestination).getList();
+    List<File> newList = ls(destination).getList();
+
     previousList.remove(file);
     newList.add(file);
 
     file.setParent(destination);
-    
-    refreshDisplays(oldDestination);
-    refreshDisplays(destination);
   }
 
-
-
-
-
   /**
-   * Query all files for a directory.
+   * Generate contents for the directory <code>root</code>
    * 
-   * @param category
-   *          the category
-   * @return the list of contacts in the category
+   * @param root
+   * @param fileNbr
+   * @param subDirNbr
    */
-  public List<File> getFilesByDirectory(File directory) {
-    return new ArrayList<File>(getFilesDataProvider(directory).getList());
-  }
+  private void generateDirContent(File root, int fileNbr, int subDirNbr) {
 
-  /**
-   * Refresh all displays.
-   */
-  public void refreshDisplays() {
-    for (ListDataProvider<File> list : filesByDirectory.values()) {
-      list.refresh();
+    assert root.getType() == FileType.DIRECTORY;
+
+    ListDataProvider<File> files = new ListDataProvider<File>();
+
+    for (int i = 0; i < subDirNbr; i++) {
+      File f = new File(FileType.DIRECTORY);
+      f.setName(root.getName() + "Event" + i);
+      f.setParent(root);
+      files.getList().add(f);
+
+      generateDirContent(f, Math.max(10, Random.nextInt(MAX_FILE_NBR)), 0);
     }
+
+    for (int i = 0; i < fileNbr; i++) {
+      File f = new File(FileType.FILE);
+      f.setName(root.getName() + "PICT" + i + ".jpg");
+      f.setParent(root);
+      files.getList().add(f);
+    }
+
+    filesByDirectory.put(root, files);
+
   }
 
   /**
-   * Refresh displays by category.
+   * Generate the file system
+   * 
+   * @param rootDirectory
    */
-  public void refreshDisplays(File d) {
-    assert d.getType() == FileType.DIRECTORY;
-    filesByDirectory.get(d).refresh();
+  private void generateDirTree(File rootDirectory) {
+    // generate user directories
+    ListDataProvider<File> files = new ListDataProvider<File>();
+
+    for (int i = 0; i < Math.max(10, Random.nextInt(20)); i++) {
+      File f = new File(FileType.DIRECTORY);
+      f.setName(nextValue(USER_DIRECTORY_NAME));
+      f.setParent(rootDirectory);
+      files.getList().add(f);
+      // generate Event sub directories
+      generateDirContent(f, 0, Math.max(10, Random.nextInt(MAX_DIR_NBR)));
+
+    }
+    filesByDirectory.put(rootDirectory, files);
+
   }
 
   /**
@@ -333,15 +326,6 @@ public class FileSystem {
    */
   private <T> T nextValue(T[] array) {
     return array[Random.nextInt(array.length)];
-  }
-
-  public ListDataProvider<File> getFilesDataProvider(File dir) {
-    return filesByDirectory.get(dir);
-  }
-
-  public ListDataProvider<File> getRootDirectories() {
-    return rootDirectories;
-    
   }
 
 }
