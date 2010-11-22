dbf8a242411344501786eb60e8bb317ade5ca2cd
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-11-22 15:23:07 +0000

    finalize GFinder sample

--- droppable/sample/src/main/java/gwtquery/plugins/droppable/client/gfindersample/FileTreeViewModel.java
+++ droppable/sample/src/main/java/gwtquery/plugins/droppable/client/gfindersample/FileTreeViewModel.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 The gwtquery plugins team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.droppable.client.gfindersample;
 
 import com.google.gwt.cell.client.AbstractCell;
@@ -17,6 +32,13 @@ import gwtquery.plugins.droppable.client.gfindersample.FileSystem.FileType;
 import gwtquery.plugins.droppable.client.gwt.CellDragAndDropBehaviour;
 import gwtquery.plugins.droppable.client.gwt.DragAndDropNodeInfo;
 
+/**
+ * Tree view model used in this example. This model use
+ * {@link DragAndDropNodeInfo} to add drag and drop support.
+ * 
+ * @author Julien Dramaix (julien.dramaix@gmail.com)
+ * 
+ */
 public class FileTreeViewModel implements TreeViewModel {
 
   /**
@@ -24,30 +46,27 @@ public class FileTreeViewModel implements TreeViewModel {
    */
   private static class FileCell extends AbstractCell<File> {
 
-
     @Override
     public void render(File value, Object key, SafeHtmlBuilder sb) {
       if (value != null) {
-        sb.appendHtmlConstant("<div class='"+getCssClassName(value)+"'>");
+        sb.appendHtmlConstant("<div class='" + getCssClassName(value) + "'>");
         sb.appendEscaped(value.getName());
         sb.appendHtmlConstant("</div>");
       }
     }
-    
-    private String getCssClassName(File value){
-      if (value.getType() == FileType.DIRECTORY){
+
+    private String getCssClassName(File value) {
+      if (value.getType() == FileType.DIRECTORY) {
         return Resource.INSTANCE.css().directoryCell();
       }
-      
+
       return Resource.INSTANCE.css().fileCell();
     }
   }
 
   private final Cell<File> fileCell;
-  private final ListDataProvider<File> rootDirectories;
 
   public FileTreeViewModel() {
-    rootDirectories = FileSystem.get().getRootDirectories();
     fileCell = new FileCell();
   }
 
@@ -56,9 +75,10 @@ public class FileTreeViewModel implements TreeViewModel {
     if (value == null) {
       // Return root directories
       DragAndDropNodeInfo<File> nodeInfo = new DragAndDropNodeInfo<File>(
-          rootDirectories, fileCell);
-      //root directory cannot be dragged
+          FileSystem.get().getRootDirectories(), fileCell);
+      // root directory cannot be dragged
       nodeInfo.setCellDroppableOnly();
+      // setup drop operation
       nodeInfo.setDroppableOptions(createDroppableOptions());
 
       return nodeInfo;
@@ -68,12 +88,18 @@ public class FileTreeViewModel implements TreeViewModel {
       File dir = (File) value;
       assert dir.getType() == FileType.DIRECTORY;
 
-      ListDataProvider<File> files = FileSystem.get().getFilesDataProvider(dir);
+      ListDataProvider<File> files = FileSystem.get().ls(dir);
+
       DragAndDropNodeInfo<File> nodeInfo = new DragAndDropNodeInfo<File>(files,
           fileCell);
+      // setup drop operation
       nodeInfo.setDroppableOptions(createDroppableOptions());
+      // setup drag operation
       nodeInfo.setDraggableOptions(createDraggableOptions());
-      nodeInfo.setCellDragAndDropBehaviour( new CellDragAndDropBehaviour<File>() {
+      // all cells can be draggable. Only cell containing a directory can be
+      // droppable
+      nodeInfo
+          .setCellDragAndDropBehaviour(new CellDragAndDropBehaviour<File>() {
 
             public boolean isDraggable(File value) {
               return true;
@@ -95,56 +121,68 @@ public class FileTreeViewModel implements TreeViewModel {
     throw new IllegalArgumentException("Unsupported object type: " + type);
   }
 
+  public boolean isLeaf(Object value) {
+    File f = (File) value;
+    return f.getType() == FileType.FILE;
+  }
+
+  /**
+   * Configure drag operation
+   * 
+   * @return
+   */
+  private DraggableOptions createDraggableOptions() {
+    DraggableOptions options = new DraggableOptions();
+    // use a clone of the original cell as drag helper
+    options.setHelper(HelperType.CLONE);
+    // set the opacity of the drag helper
+    options.setOpacity((float) 0.8);
+    // set the cursor to use during the drag operation
+    options.setCursor(Cursor.POINTER);
+    // append the drag helper to the CellBrowser
+    options.setAppendTo("#fileBrowser");
+    // constrain the drag operation inside the CellBrowser
+    options.setContainment("#fileBrowser");
+
+    return options;
+  }
+
+  /**
+   * Configure drop operation
+   * 
+   * @return
+   */
   private DroppableOptions createDroppableOptions() {
     DroppableOptions options = new DroppableOptions();
-    //css class set to the droppable when an acceptable draggable is over it
+    // css class set to the droppable when an acceptable draggable is over it
     options.setDroppableHoverClass(Resource.INSTANCE.css().droppableHover());
-  //css class set to the draggable when it is over a droppable
+    // css class set to the draggable when it is over a droppable
     options.setDraggableHoverClass(Resource.INSTANCE.css().dragHover());
-    
-    //a directory cannot be drop on one of these children
+
+    // a directory cannot be drop in one of these children
     options.setAccept(new AcceptFunction() {
-      
+
       public boolean acceptDrop(DragAndDropContext ctx) {
         File dropDirectory = ctx.getDroppableData();
         File dragFile = ctx.getDraggableData();
-        if (dragFile.getType() == FileType.FILE){
+        if (dragFile.getType() == FileType.FILE) {
           return true;
         }
-        
-        //check that the dropDirectory is not a child of dragFile
+
+        // check that the dropDirectory is not a child of dragFile
         File parent = dropDirectory.getParent();
-        while( parent != null){
-          if (parent.equals(dragFile)){
+        while (parent != null) {
+          if (parent.equals(dragFile)) {
             return false;
           }
           parent = parent.getParent();
         }
-        
+
         return true;
       }
     });
-    
-    return options;
-  }
 
-  private DraggableOptions createDraggableOptions() {
-    DraggableOptions options = new DraggableOptions();
-    options.setHelper(HelperType.CLONE);
-    options.setOpacity((float) 0.8);
-    options.setCursor(Cursor.POINTER);
-    options.setAppendTo("#fileBrowser");
-   // options.setContainment("#fileBrowser");
-    //bug in the position helper when cellbrowser is scrolling
-    //workaround : force the position of the helper to be on the pointer
-    //options.setCursorAt(new CursorAt(5, 5, null, null));
-    // draggableFileCell.setDistance(0);
     return options;
-  }
-
-  public boolean isLeaf(Object value) {
-    File f = (File) value;
-    return f.getType() == FileType.FILE;
   }
 
 }
