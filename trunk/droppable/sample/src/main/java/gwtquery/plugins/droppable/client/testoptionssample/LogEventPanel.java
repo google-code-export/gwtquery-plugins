db638194430711560547a9ebcb9b81b502e3665a
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-11-23 21:21:21 +0000

    finalize testoptionssample

--- trunk/droppable/sample/src/main/java/gwtquery/plugins/droppable/client/testoptionssample/LogEventPanel.java
+++ trunk/droppable/sample/src/main/java/gwtquery/plugins/droppable/client/testoptionssample/LogEventPanel.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 The gwtquery plugins team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.droppable.client.testoptionssample;
 
 import static com.google.gwt.query.client.GQuery.$;
@@ -34,6 +49,13 @@ import gwtquery.plugins.droppable.client.events.DropEvent.DropEventHandler;
 import gwtquery.plugins.droppable.client.events.OutDroppableEvent.OutDroppableEventHandler;
 import gwtquery.plugins.droppable.client.events.OverDroppableEvent.OverDroppableEventHandler;
 
+/**
+ * Panel displaying log of the different events that occur during a drag and
+ * drop operation
+ * 
+ * @author Julien Dramaix (julien.dramaix@gmail.com)
+ * 
+ */
 public class LogEventPanel extends Composite implements
     BeforeDragStartEventHandler, DragStartEventHandler, DragStopEventHandler,
     DragEventHandler, DropEventHandler, DeactivateDroppableEventHandler,
@@ -44,37 +66,37 @@ public class LogEventPanel extends Composite implements
   interface LogEventPanelUiBinder extends UiBinder<Widget, LogEventPanel> {
   }
 
+  private static String HIDE_WINDOW_EVENT = "Hide log window";
+
+  private static String SHOW_WINDOW_EVENT = "Show log window";
   private static LogEventPanelUiBinder uiBinder = GWT
       .create(LogEventPanelUiBinder.class);
 
-  private static String SHOW_WINDOW_EVENT = "Show log window";
-  private static String HIDE_WINDOW_EVENT = "Hide log window";
-
-  @UiField
-  Button windowToogleButton;
   @UiField
   Button clearLogButton;
   @UiField
-  CheckBox enableBeforeDragStart;
-  @UiField
-  CheckBox enableDragStart;
-  @UiField
-  CheckBox enableDragMove;
-  @UiField
-  CheckBox enableDragStop;
-  @UiField
   CheckBox enableActivateDrop;
+  @UiField
+  CheckBox enableBeforeDragStart;
   @UiField
   CheckBox enableDeactivateDrop;
   @UiField
-  CheckBox enableOverDrop;
+  CheckBox enableDragMove;
+  @UiField
+  CheckBox enableDragStart;
+  @UiField
+  CheckBox enableDragStop;
+  @UiField
+  CheckBox enableDrop;
   @UiField
   CheckBox enableOutDrop;
   @UiField
-  CheckBox enableDrop;
-
+  CheckBox enableOverDrop;
   @UiField
   DivElement logEventWindow;
+
+  @UiField
+  Button windowToogleButton;
 
   private boolean windowOpen = true;
 
@@ -85,31 +107,81 @@ public class LogEventPanel extends Composite implements
     initCheckBoxes();
   }
 
-  private void initCheckBoxes() {
-    enableBeforeDragStart.setValue(true);
-    enableDragStart.setValue(true);
-    enableDrop.setValue(true);
-    enableDragStop.setValue(true);
-    enableActivateDrop.setValue(true);
-    enableDeactivateDrop.setValue(true);
-    enableOverDrop.setValue(true);
-    enableOutDrop.setValue(true);
-    enableDrop.setValue(true);
-  }
-
-  private void toogleWindowLog() {
-    if (windowOpen) {
-      $("#logRow").hide();
-      $(clearLogButton.getElement()).hide();
-      windowOpen = false;
-      windowToogleButton.setText(SHOW_WINDOW_EVENT);
-    } else {
-      $("#logRow").show();
-      $(clearLogButton.getElement()).show();
-      windowOpen = true;
-      windowToogleButton.setText(HIDE_WINDOW_EVENT);
+  public void onActivateDroppable(ActivateDroppableEvent event) {
+    if (isActivateDropLoggingEnable()) {
+      log("Activate droppable " + event.getDroppable().getId());
     }
 
+  }
+
+  public void onBeforeDragStart(BeforeDragStartEvent event) {
+    if (isBeforeDragStartLoggingEnable()) {
+      log("The drag operation will be started for draggable "
+          + event.getDraggable().getId());
+    }
+
+  }
+
+  @UiHandler(value = { "clearLogButton" })
+  public void onClearLogClicked(ClickEvent e) {
+    $(logEventWindow).html("");
+    logEventWindow.setScrollTop(0);
+  }
+
+  public void onDeactivateDroppable(DeactivateDroppableEvent event) {
+    if (isDeactivateDropLoggingEnable()) {
+      log("Deactivate droppable " + event.getDroppable().getId());
+    }
+
+  }
+
+  public void onDrag(DragEvent event) {
+    if (isDragMoveLoggingEnable()) {
+      log("Dragging draggable " + event.getDraggable().getId());
+    }
+
+  }
+
+  public void onDragStart(DragStartEvent event) {
+    if (isDragStartLoggingEnable()) {
+      log("Drag started for draggable " + event.getDraggable().getId());
+    }
+
+  }
+
+  public void onDragStop(DragStopEvent event) {
+    if (isDragStopLoggingEnable()) {
+      log("Drag stopped for draggable " + event.getDraggable().getId());
+    }
+    log("==========================");
+
+  }
+
+  public void onDrop(DropEvent event) {
+    if (isDropLoggingEnable()) {
+      log("The draggable " + event.getDraggable().getId()
+          + " was drop in the droppable " + event.getDroppable().getId());
+    }
+
+  }
+
+  public void onOutDroppable(OutDroppableEvent event) {
+    if (isOutDropLoggingEnable()) {
+      log("The draggable " + event.getDraggable().getId()
+          + " is out the droppable " + event.getDroppable().getId());
+    }
+  }
+
+  public void onOverDroppable(OverDroppableEvent event) {
+    if (isOverDropLoggingEnable()) {
+      log("The draggable " + event.getDraggable().getId()
+          + " is over the droppable " + event.getDroppable().getId());
+    }
+  }
+
+  @UiHandler(value = { "windowToogleButton" })
+  public void onToogleWindowClicked(ClickEvent e) {
+    toogleWindowLog();
   }
 
   private void bind() {
@@ -125,6 +197,30 @@ public class LogEventPanel extends Composite implements
 
   }
 
+  private void initCheckBoxes() {
+    enableBeforeDragStart.setValue(true);
+    enableDragStart.setValue(true);
+    enableDrop.setValue(true);
+    enableDragStop.setValue(true);
+    enableActivateDrop.setValue(true);
+    enableDeactivateDrop.setValue(true);
+    enableOverDrop.setValue(true);
+    enableOutDrop.setValue(true);
+    enableDrop.setValue(true);
+  }
+
+  private boolean isActivateDropLoggingEnable() {
+    return enableActivateDrop.getValue();
+  }
+
+  private boolean isBeforeDragStartLoggingEnable() {
+    return enableBeforeDragStart.getValue();
+  }
+
+  private boolean isDeactivateDropLoggingEnable() {
+    return enableDeactivateDrop.getValue();
+  }
+
   private boolean isDragMoveLoggingEnable() {
     return enableDragMove.getValue();
   }
@@ -133,20 +229,12 @@ public class LogEventPanel extends Composite implements
     return enableDragStart.getValue();
   }
 
-  private boolean isBeforeDragStartLoggingEnable() {
-    return enableBeforeDragStart.getValue();
-  }
-
   private boolean isDragStopLoggingEnable() {
     return enableDragStop.getValue();
   }
 
-  private boolean isActivateDropLoggingEnable() {
-    return enableActivateDrop.getValue();
-  }
-
-  private boolean isDeactivateDropLoggingEnable() {
-    return enableDeactivateDrop.getValue();
+  private boolean isDropLoggingEnable() {
+    return enableDrop.getValue();
   }
 
   private boolean isOutDropLoggingEnable() {
@@ -157,10 +245,6 @@ public class LogEventPanel extends Composite implements
     return enableOverDrop.getValue();
   }
 
-  private boolean isDropLoggingEnable() {
-    return enableDrop.getValue();
-  }
-
   private void log(String log) {
     String innerHtml = $(logEventWindow).html();
     innerHtml += log + "<br />";
@@ -169,78 +253,17 @@ public class LogEventPanel extends Composite implements
 
   }
 
-  @UiHandler(value = { "windowToogleButton" })
-  public void onToogleWindowClicked(ClickEvent e) {
-    toogleWindowLog();
-  }
-
-  @UiHandler(value = { "clearLogButton" })
-  public void onClearLogClicked(ClickEvent e) {
-    $(logEventWindow).html("");
-    logEventWindow.setScrollTop(0);
-  }
-
-  public void onDragStart(DragStartEvent event) {
-    if (isDragStartLoggingEnable()){
-      log("Drag started for draggable " + event.getDraggable().getId());
-    }
-
-  }
-
-  public void onDragStop(DragStopEvent event) {
-    if (isDragStopLoggingEnable()){
-      log("Drag stopped for draggable " + event.getDraggable().getId());
-    }
-    log("==========================");
-
-  }
-
-  public void onDrag(DragEvent event) {
-    if (isDragMoveLoggingEnable()) {
-      log("Dragging draggable " + event.getDraggable().getId());
-    }
-
-  }
-
-  public void onDrop(DropEvent event) {
-    if (isDropLoggingEnable()){
-      log("The draggable " + event.getDraggable().getId()
-        + " was drop in the droppable " + event.getDroppable().getId());
-    }
-
-  }
-
-  public void onDeactivateDroppable(DeactivateDroppableEvent event) {
-    if (isDeactivateDropLoggingEnable()){
-      log("Deactivate droppable " + event.getDroppable().getId());
-    }
-
-  }
-
-  public void onActivateDroppable(ActivateDroppableEvent event) {
-    if (isActivateDropLoggingEnable()){
-      log("Activate droppable " + event.getDroppable().getId());
-    }
-
-  }
-
-  public void onOutDroppable(OutDroppableEvent event) {
-    if (isOutDropLoggingEnable()){
-      log("The draggable " + event.getDraggable().getId()
-        + " is out the droppable " + event.getDroppable().getId());
-    }
-  }
-
-  public void onOverDroppable(OverDroppableEvent event) {
-    if (isOverDropLoggingEnable()){
-      log("The draggable " + event.getDraggable().getId()
-        + " is over the droppable " + event.getDroppable().getId());
-    }
-  }
-
-  public void onBeforeDragStart(BeforeDragStartEvent event) {
-    if (isBeforeDragStartLoggingEnable()){
-      log("The drag operation will be initialized for draggable "+event.getDraggable().getId()); 
+  private void toogleWindowLog() {
+    if (windowOpen) {
+      $("#logRow").hide();
+      $(clearLogButton.getElement()).hide();
+      windowOpen = false;
+      windowToogleButton.setText(SHOW_WINDOW_EVENT);
+    } else {
+      $("#logRow").show();
+      $(clearLogButton.getElement()).show();
+      windowOpen = true;
+      windowToogleButton.setText(HIDE_WINDOW_EVENT);
     }
 
   }
