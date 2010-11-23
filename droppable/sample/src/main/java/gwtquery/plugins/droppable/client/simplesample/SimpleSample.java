edf18e7fe95ee761d60ce2c717ea7a277453f021
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-11-23 20:59:45 +0000

    finalize simple sample

--- droppable/sample/src/main/java/gwtquery/plugins/droppable/client/simplesample/SimpleSample.java
+++ droppable/sample/src/main/java/gwtquery/plugins/droppable/client/simplesample/SimpleSample.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 The gwtquery plugins team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.droppable.client.simplesample;
 
 import static com.google.gwt.query.client.GQuery.$;
@@ -10,6 +25,12 @@ import gwtquery.plugins.droppable.client.DroppableOptions;
 import gwtquery.plugins.droppable.client.DroppableOptions.DroppableFunction;
 import gwtquery.plugins.droppable.client.events.DragAndDropContext;
 
+/**
+ * Make any elements droppable !
+ * 
+ * @author Julien Dramaix (julien.dramaix@gmail.com)
+ * 
+ */
 public class SimpleSample implements EntryPoint {
 
   public void onModuleLoad() {
@@ -21,16 +42,12 @@ public class SimpleSample implements EntryPoint {
 
   private DroppableOptions createDroppableOptions() {
     DroppableOptions options = new DroppableOptions();
-    options.setHoverClass("yellow-background");
+    // class added to the droppable when a acceptable draggable is over it
+    options.setDroppableHoverClass("yellow-background");
+    // class added to the droppable when the droppable is activated
     options.setActiveClass("white-background");
-    options.setOnDrop(new DroppableFunction() {
-      public void f(DragAndDropContext context) {
-        $("p", context.getDroppable()).html(
-            "Element " + context.getDraggable().getId() + " was drop on me");
-        context.getDroppable().addClassName("orange-background");
 
-      }
-    });
+    // function called on when a droppable is activated
     options.setOnActivate(new DroppableFunction() {
 
       public void f(DragAndDropContext context) {
@@ -39,6 +56,18 @@ public class SimpleSample implements EntryPoint {
 
       }
     });
+
+    // function called on when a acceptable draggable is dropped on the droppable
+    options.setOnDrop(new DroppableFunction() {
+      public void f(DragAndDropContext context) {
+        $("p", context.getDroppable()).html(
+            "Element " + context.getDraggable().getId() + " was drop on me");
+        context.getDroppable().addClassName("orange-background");
+
+      }
+    });
+
+    // function called on when a droppable is deactivated
     options.setOnDeactivate(new DroppableFunction() {
 
       public void f(DragAndDropContext context) {
@@ -46,15 +75,20 @@ public class SimpleSample implements EntryPoint {
 
       }
     });
+
+    // function called on when a acceptable draggable is dragged over the
+    // droppable
     options.setOnOver(new DroppableFunction() {
-      
+
       public void f(DragAndDropContext context) {
         $("p", context.getDroppable()).html(
-            "The element " + context.getDraggable().getId() + " is over me and can be dropped");
+            "The element " + context.getDraggable().getId()
+                + " is over me and can be dropped");
 
       }
     });
 
+    // function called on when a acceptable draggable is dragged out the droppable
     options.setOnOut(new DroppableFunction() {
       public void f(DragAndDropContext context) {
         $("p", context.getDroppable()).html("I'm ready to accept an element");
