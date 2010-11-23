34547b38c556f50e5b2f88c6e43efd88ee7e1402
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-11-23 19:41:56 +0000

    finalize portlets sample

--- droppable/sample/src/main/java/gwtquery/plugins/droppable/client/gwtportletsample/DroppablePanel.java
+++ droppable/sample/src/main/java/gwtquery/plugins/droppable/client/gwtportletsample/DroppablePanel.java
@@ -1,11 +1,30 @@
+/*
+ * Copyright 2010 The gwtquery plugins team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package gwtquery.plugins.droppable.client.gwtportletsample;
-
-import static com.google.gwt.query.client.GQuery.$;
 
 import com.google.gwt.user.client.ui.FlowPanel;
 
 import gwtquery.plugins.droppable.client.gwt.DroppableWidget;
 
+/**
+ * Droppable Panel containing the portlets.
+ * 
+ * @author Julien Dramaix (julien.dramaix@gmail.com)
+ *
+ */
 public class DroppablePanel extends DroppableWidget<FlowPanel> {
 
   private FlowPanel innerPanel;
@@ -22,17 +41,18 @@ public class DroppablePanel extends DroppableWidget<FlowPanel> {
 
   private void init() {
     innerPanel = new FlowPanel();
-    // init style with GQuery
-    $(innerPanel).css("paddingBottom", "100px").css("width", "200px").css(
-        "float", "left").css("marginLeft", "10px");
+    innerPanel.addStyleName(Resources.INSTANCE.css().sortablePanel());
 
   }
 
+  /**
+   * Register drop handler !
+   */
   private void setupDrop() {
-    DragAndDropHandler handler = new DragAndDropHandler(innerPanel);
-    addDropHandler(handler);
-    addOutDroppableHandler(handler);
-    addOverDroppableHandler(handler);
+    SortableDragAndDropHandler sortableHandler = new SortableDragAndDropHandler(innerPanel);
+    addDropHandler(sortableHandler);
+    addOutDroppableHandler(sortableHandler);
+    addOverDroppableHandler(sortableHandler);
   }
 
 }
