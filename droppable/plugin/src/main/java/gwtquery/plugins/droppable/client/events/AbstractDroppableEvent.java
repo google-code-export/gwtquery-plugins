0eb061cbfb9beb614b34325d8b316eafbb7f8e56
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2010-11-25 22:33:51 +0000

    add getDragHelper method

--- droppable/plugin/src/main/java/gwtquery/plugins/droppable/client/events/AbstractDroppableEvent.java
+++ droppable/plugin/src/main/java/gwtquery/plugins/droppable/client/events/AbstractDroppableEvent.java
@@ -120,4 +120,13 @@ public abstract class AbstractDroppableEvent<H extends EventHandler> extends
     assert dragAndDropContext != null : "DragAndDropContext cannot be null";
     return dragAndDropContext.getDroppableWidget();
   }
+  
+  /**
+   * 
+   * @return the DOM element used for dragging display
+   */
+  public Element getDragHelper() {
+    assert dragAndDropContext != null : "DragAndDropContext cannot be null";
+    return dragAndDropContext.getHelper();
+  }
 }
