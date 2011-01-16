0d06b0d37dba81b262b23860b1599e9096fd2ebb
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2011-01-16 19:27:10 +0000

    GwtQueryUi moved to the GwtQuery trunk

--- trunk/selectable/plugin/src/main/java/gwtquery/plugins/selectable/client/event/SelectingEvent.java
+++ trunk/selectable/plugin/src/main/java/gwtquery/plugins/selectable/client/event/SelectingEvent.java
@@ -22,7 +22,7 @@ import com.google.gwt.event.shared.GwtEvent;
 /**
  * Event fired when an element is selecting.
  * 
- * @author Julien Dramaix (julien.dramaix@gmail.com)
+ * @author Julien Dramaix (julien.dramaix@gmail.com, @jdramaix)
  * 
  */
 public class SelectingEvent extends
