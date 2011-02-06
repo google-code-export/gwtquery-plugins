48315ac19ab9a0f802a657e798f694d815ef623c
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2011-02-06 20:45:48 +0000

    Create the 1.0 branch of the draggable plugin

---
+++ branches/draggable_1_0/plugin/src/main/java/gwtquery/plugins/draggable/client/events/DragStopEvent.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2010 The gwtquery plugins team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package gwtquery.plugins.draggable.client.events;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.shared.EventHandler;
+
+/**
+ * Event fired when the drag operation stops.
+ * 
+ * @author Julien Dramaix (julien.dramaix@gmail.com)
+ * 
+ */
+public class DragStopEvent extends AbstractDraggableEvent<DragStopEvent.DragStopEventHandler> {
+
+  public interface DragStopEventHandler extends EventHandler {
+    public void onDragStop(DragStopEvent event);
+  }
+
+  public static Type<DragStopEventHandler> TYPE = new Type<DragStopEventHandler>();
+
+
+  public DragStopEvent(Element draggable) {
+    super(draggable);
+  }
+
+  @Override
+  public Type<DragStopEventHandler> getAssociatedType() {
+    return TYPE;
+  }
+
+  @Override
+  protected void dispatch(DragStopEventHandler handler) {
+    handler.onDragStop(this);
+  }
+
+}
