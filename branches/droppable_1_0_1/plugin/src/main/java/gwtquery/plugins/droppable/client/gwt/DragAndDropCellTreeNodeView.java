bd4257a6ee78816082c2a6a2bf9198cb3a615e0b
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2011-02-16 18:02:16 +0000

    remove log

--- branches/droppable_1_0_1/plugin/src/main/java/gwtquery/plugins/droppable/client/gwt/DragAndDropCellTreeNodeView.java
+++ branches/droppable_1_0_1/plugin/src/main/java/gwtquery/plugins/droppable/client/gwt/DragAndDropCellTreeNodeView.java
@@ -55,7 +55,6 @@ class DragAndDropCellTreeNodeView<T> extends CellTreeNodeView<T> {
       public View(Element childContainer) {
         
         super(childContainer);
-        GWT.log("Enter in my view");
         this.childContainer = childContainer;
       }
 
@@ -138,7 +137,7 @@ class DragAndDropCellTreeNodeView<T> extends CellTreeNodeView<T> {
     public DragAndDropNodeCellList(final NodeInfo<C> nodeInfo,
         final CellTreeNodeView<?> nodeView, int pageSize) {
       super(nodeInfo, nodeView, pageSize);
-      GWT.log("enter in my node cellList");
+     
     }
 
     /**
