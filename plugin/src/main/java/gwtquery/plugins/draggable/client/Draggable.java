bf6661b68f3ee9d961279676a88351dfb7d60f3b
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2011-03-02 18:59:09 +0000

    plugin class moved

--- plugin/src/main/java/gwtquery/plugins/draggable/client/Draggable.java
+++ plugin/src/main/java/gwtquery/plugins/draggable/client/Draggable.java
@@ -26,7 +26,7 @@ import com.google.gwt.event.shared.UmbrellaException;
 import com.google.gwt.query.client.Function;
 import com.google.gwt.query.client.GQuery;
 import com.google.gwt.query.client.JSArray;
-import com.google.gwt.query.client.Plugin;
+import com.google.gwt.query.client.plugins.Plugin;
 
 import gwtquery.plugins.commonui.client.Event;
 import gwtquery.plugins.commonui.client.MouseHandler;
