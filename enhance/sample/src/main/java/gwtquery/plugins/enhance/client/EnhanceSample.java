04a6d53c545f2862bbbbebd9262f489f4f5dd0da
manuel.carrasco.m <manuel.carrasco.m@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2011-07-06 16:01:54 +0000

    Fixes issue15

--- enhance/sample/src/main/java/gwtquery/plugins/enhance/client/EnhanceSample.java
+++ enhance/sample/src/main/java/gwtquery/plugins/enhance/client/EnhanceSample.java
@@ -3,27 +3,49 @@ package gwtquery.plugins.enhance.client;
 import static com.google.gwt.query.client.GQuery.$;
 import static gwtquery.plugins.enhance.client.Enhance.Enhance;
 import gwtquery.plugins.enhance.client.colorpicker.ColorPickerFactory.ColorPickerType;
-import gwtquery.plugins.enhance.client.multiselect.MultiSelect;
 import gwtquery.plugins.enhance.client.slider.SliderBar;
 import gwtquery.plugins.enhance.client.slider.SliderBar.LabelFormatter;
 
 import java.util.Arrays;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.query.client.Function;
 import com.google.gwt.query.client.plugins.widgets.WidgetInitializer;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 /**
  * Example code for the GwtQuery Enhance plugin.
  */
 public class EnhanceSample implements EntryPoint {
   
   public void onModuleLoad() {
+    
+    TextBox t = new TextBox();
+    t.addValueChangeHandler(new ValueChangeHandler<String>() {
+      public void onValueChange(ValueChangeEvent<String> event) {
+        GWT.log("event.getValue():" + event.getValue());
+        GWT.log("event.getSource():" + event.getSource());
+      }
+    });
+    SliderBar slider = new SliderBar(0, 10);
+    slider.setStepSize(1);
+    ValueChangeHandler<Double> v = new ValueChangeHandler<Double>() {
+      public void onValueChange(ValueChangeEvent<Double> event) {
+        System.out.println("Change");
+        GWT.log("event.getValue():" + event.getValue());
+        GWT.log("event.getSource():" + event.getSource());
+      }
+    };
+    slider.addValueChangeHandler(v);
+
+    RootPanel.get().add(t);
+    RootPanel.get().add(slider);
+    if (true) return;
     $("#enhance").click(new Function(){
       public void f(Element e) {
         $(".editable").as(Enhance).richText();
