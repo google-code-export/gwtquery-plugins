96cef8d2950d2f714cb9f1e1043fbf4fa7eed11f
julien.dramaix@gmail.com <julien.dramaix@gmail.com@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2011-11-13 22:04:10 +0000

    GWT 2.4 compliant

---
+++ branches/droppable_1_0/plugin/src/main/java/gwtquery/plugins/droppable/client/gwt/DragAndDropAbstractCellTableDelegate.java
@@ -0,0 +1,11 @@
+package gwtquery.plugins.droppable.client.gwt;
+
+/**
+ * Class used internally by {@link DragAndDropCellTable} and {@link DragAndDropDataGrid}
+ * 
+ * @author jDramaix
+ *
+ */
+class DragAndDropAbstractCellTableDelegate {
+
+}
