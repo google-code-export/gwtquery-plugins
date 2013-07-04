f1a0b26f2421ead25d0caf270a67136aff2daee2
manuel.carrasco.m <manuel.carrasco.m@4cdfd5ed-8f87-bf3d-f74d-4402eda93d27>
2013-07-04 07:56:20 +0000

    Update demos

--- enhance/demos/EnhanceGwt/deferredjs/47CDDF86350535534A1B9184BC38B72C/1.cache.js
+++
@@ -1,4 +0,0 @@
-function Au(){}
-function yu(){}
-function Cu(){zu=new Au;Kb((Ib(),Hb),1);!!$stats&&$stats(mc(D1,E1,-1,-1));zu.Cb();!!$stats&&$stats(mc(D1,F1,-1,-1))}
-var D1='runCallbacks1';_=Au.prototype=yu.prototype=new I;_.gC=function Bu(){return Go};_.Cb=function Du(){};_.cM={};var zu=null;var Go=$T('com.google.gwt.lang.asyncloaders.','AsyncLoader1');$Y(Cu)();
--- enhance/demos/EnhanceGwt/deferredjs/E8EB63EC53F8B384B38462B548CE19EB/1.cache.js
+++
@@ -1,4 +0,0 @@
-function Fu(){}
-function Du(){}
-function Hu(){Eu=new Fu;Kb((Ib(),Hb),1);!!$stats&&$stats(mc(D1,E1,-1,-1));Eu.yb();!!$stats&&$stats(mc(D1,F1,-1,-1))}
-var D1='runCallbacks1';_=Fu.prototype=Du.prototype=new I;_.gC=function Gu(){return Mo};_.yb=function Iu(){};_.cM={};var Eu=null;var Mo=aU('com.google.gwt.lang.asyncloaders.','AsyncLoader1');aZ(Hu)();
